9dc102a13639d9e30f15128458001a088455d70a
dreamer2908 <dreamer2908@gmail.com>
2014-10-02 23:29:40 +0700

    Add Gauss_Seidel.solve and 3 test cases from Wikipedia. All passed.

---
+++ Gauss-Seidel Serial/Gauss_Seidel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gauss_Seidel_Serial
+{
+    class Gauss_Seidel
+    {
+        public static Matrix solve(Matrix A, Matrix b)
+        {
+            // check sanity
+            if (!A.isSquare() || !b.isColumn() || (A.Height != b.Height))
+            {
+                Exception e = new Exception("Matrix A must be square! Matrix b must be a column matrix with the same height as matrix A!");
+                throw e;
+            }
+
+            int ITERATION_LIMIT = 1000;
+
+            Matrix x = Matrix.zeroLike(b);
+            Matrix L, U, L_1, T, C, new_x;
+            Matrix.Decompose(A, out L, out U);
+            L_1 = ~L;
+            T = -L_1 * U;
+            C = L_1 * b;
+
+            int loops = 0;
+            for (; loops < ITERATION_LIMIT; loops++)
+            {
+                new_x = T * x + C;
+                if (Matrix.AllClose(new_x, x, 1e-8))
+                    break;
+                x = new_x;
+            }
+
+            return x;
+        }
+    }
+}
