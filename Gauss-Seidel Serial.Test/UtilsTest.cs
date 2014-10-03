5564595661fb60c641731b72d5c90a9492fde056
dreamer2908 <dreamer2908@gmail.com>
2014-10-03 20:57:14 +0700

    Added a temporary parser (Utils.parseInput) and a test for it

---
+++ Gauss-Seidel Serial.Test/UtilsTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Gauss_Seidel_Serial.Test
+{
+    [TestFixture]
+    class UtilsTest
+    {
+        [Test]
+        public void parseInput_Sample1_ChecksThem()
+        {
+            string sample = "4\n10 -1 2 0\n-1 11 -1 3\n2 -1 10 -1\n0 3 -1 8\n6 25 -11 15\n1 2 -1 1\n-------------";
+            Matrix A, b, sol;
+            Boolean re = Utils.parseInput(sample, out A, out b, out sol);
+            try
+            {
+                Console.WriteLine("Matrix A:");
+                Console.WriteLine(A.ToString());
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                Console.WriteLine("Matrix b:");
+                Console.WriteLine(b.ToString());
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                Console.WriteLine("Matrix sol:");
+                Console.WriteLine(sol.ToString());
+            }
+            catch (Exception)
+            {
+            }
+
+            Matrix _A = new Matrix(4, 4);
+            _A[0, 0] = 10;
+            _A[0, 1] = -1;
+            _A[0, 2] = 2;
+            _A[0, 3] = 0;
+            _A[1, 0] = -1;
+            _A[1, 1] = 11;
+            _A[1, 2] = -1;
+            _A[1, 3] = 3;
+            _A[2, 0] = 2;
+            _A[2, 1] = -1;
+            _A[2, 2] = 10;
+            _A[2, 3] = -1;
+            _A[3, 0] = 0;
+            _A[3, 1] = 3;
+            _A[3, 2] = -1;
+            _A[3, 3] = 8;
+            Matrix _b = new Matrix(4, 1);
+            _b[0, 0] = 6;
+            _b[1, 0] = 25;
+            _b[2, 0] = -11;
+            _b[3, 0] = 15;
+            Matrix _sol = new Matrix(4, 1);
+            _sol[0, 0] = 1;
+            _sol[1, 0] = 2;
+            _sol[2, 0] = -1;
+            _sol[3, 0] = 1;
+
+            Assert.AreEqual(true, re);
+            Assert.AreEqual(_A.ToString(), A.ToString());
+            Assert.AreEqual(_b.ToString(), b.ToString());
+            Assert.AreEqual(_sol.ToString(), sol.ToString());
+        }
+    }
+}
---
+++ Gauss-Seidel Serial/Utils.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gauss_Seidel_Serial
+{
+    class Utils
+    {
+        public static Boolean parseInput(string input, out Matrix A, out Matrix b, out Matrix sol)
+        {
+            // split input into lines. All empty lines are removed
+            string[] inputArray = input.Split(new char[] {'\n', '\r'},  StringSplitOptions.RemoveEmptyEntries);
+            // assume its format is correct FOR NOW
+            int offset = 0; // loop later
+            int size = 0;
+            if (int.TryParse(inputArray[offset], out size))
+            {
+                A = new Matrix(size, size); // size of A = number of entries on each line
+                for (int i = 0; i < size; i++)
+                {
+                    string line = inputArray[offset + i + 1];
+                    string[] entries = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    for (int j = 0; j < size; j++)
+                    {
+                        Double number;
+                        if (Double.TryParse(entries[j], out number))
+                            A[i, j] = number;
+                        else
+                            A[i, j] = 0;
+                    }
+                }
+                // now RHS vector
+                string RHSLine = inputArray[offset + size + 1];
+                string[] RHSEntries = RHSLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                b = new Matrix(size, 1);
+                for (int i = 0; i < size; i++)
+                {
+                    Double number;
+                    if (Double.TryParse(RHSEntries[i], out number))
+                        b[i, 0] = number;
+                    else
+                        b[i, 0] = 0;
+                }
+                // now solution vector
+                string solLine = inputArray[offset + size + 2];
+                if (!solLine.StartsWith("--"))
+                {
+                    string[] solEntries = solLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    sol = new Matrix(size, 1);
+                    for (int i = 0; i < size; i++)
+                    {
+                        Double number;
+                        if (Double.TryParse(solEntries[i], out number))
+                            sol[i, 0] = number;
+                        else
+                            sol[i, 0] = 0;
+                    }
+                }
+                else
+                {
+                    sol = null;
+                }
+                return true;
+            }
+            else
+            {
+                A = null;
+                b = null;
+                sol = null;
+                return false;
+            }
+        }
+    }
+}
