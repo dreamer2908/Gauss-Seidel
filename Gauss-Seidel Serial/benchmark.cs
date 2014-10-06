c41c4ff6286c0bd93240634c00beddd3fa17a80b
dreamer2908 <dreamer2908@gmail.com>
2014-10-06 16:42:47 +0700

    Unimportant

---
+++ Gauss-Seidel Serial/benchmark.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Hill_Cipher.Research
+{
+    class benchmark
+    {
+        public benchmark()
+        {
+            stopWatch = new Stopwatch();
+        }
+
+        Stopwatch stopWatch;
+
+        public void startBenchmark()
+        {
+            stopWatch.Reset();
+            stopWatch.Start();
+        }
+
+        public string getElapsedTime()
+        {
+            TimeSpan ts = stopWatch.Elapsed;
+            return format(ts);
+        }
+
+        public string getBenchmarkResult()
+        {
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            return format(ts);
+        }
+
+        private string format(TimeSpan ts)
+        {
+            return (String.Format("{0:00}:{1:00}:{2:00}.{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds));
+        }
+    }
+}
